a6b9828f74a3cc4c47bdd06abc9d9e5530dbc461
msyukhaingmarmyint <khaingkhainglay984@gmail.com>
2026-07-03 11:06:36 +0630

    Modify dashboard section in admin side

--- frontend-react/src/pages/admin/Dashboard.jsx
+++ frontend-react/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,212 @@
-const Dashboard = () => {
-  return (
-    <div>
-      Admin Dashboard
-    </div>
-  )
-}
+import { CalendarDays, Banknote, Wallet, QrCode, ChartColumn, TriangleAlert, ShieldCheck, RotateCcw, } from "lucide-react";
 
-export default Dashboard
+const stats = [
+  {
+    title: "TODAY TOTAL SALES",
+    amount: 66780,
+    icon: Banknote,
+    bg: "bg-emerald-100",
+    color: "text-emerald-600",
+  },
+  {
+    title: "TOTAL CASH RECEIVED",
+    amount: 46780,
+    icon: Wallet,
+    bg: "bg-blue-100",
+    color: "text-blue-600",
+  },
+  {
+    title: "TOTAL KPAY RECEIVED",
+    amount: 20000,
+    icon: QrCode,
+    bg: "bg-purple-100",
+    color: "text-purple-600",
+  },
+  {
+    title: "TOTAL WAVEPAY RECEIVED",
+    amount: 0,
+    icon: QrCode,
+    bg: "bg-purple-100",
+    color: "text-purple-600",
+  },
+];
+
+const products = [
+  {
+    name: "Premier Coffee Mix 30s",
+    qty: 20,
+    stock : 8,
+    color: "bg-orange-500",
+  },
+  {
+    name: "Nestlé Milo Powder",
+    qty: 15,
+    stock : 12,
+    color: "bg-emerald-500",
+  },
+  {
+    name: "Pringles Original",
+    qty: 10,
+    stock : 8,
+    color: "bg-cyan-500",
+  },
+  {
+    name: "Tissue Soft Roll (Pack of 10)",
+    qty: 5,
+    stock : 5,
+    color: "bg-teal-500",
+  },
+
+  {
+    name: "Oreo Chocolate Biscuit",
+    qty: 8,
+    stock : 3,
+    color: "bg-amber-500",
+  },
+];
+
+const sortedProducts = [...products].sort((a, b) => b.qty - a.qty);
+const totalQty = sortedProducts.reduce((sum, item) => sum + item.qty, 0);
+const lowStockProducts = products.filter((item) => item.stock <= 5);
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen">
+
+      {/* Header Section*/}
+      <div className="bg-white rounded-2xl border shadow-sm p-6 flex flex-col lg:flex-row justify-between gap-6">
+        <div className="flex items-center gap-4">
+          <div className="w-12 h-12 rounded-xl bg-emerald-100 flex items-center justify-center">
+            <CalendarDays className="w-6 h-6 text-slate-600" />
+          </div>
+
+          <div>
+            <h1 className="font-bold text-xl">Sales Analytics (Date Filter)</h1>
+            <p className="text-sm text-slate-500">Filter sales reports based on the selected date range.</p>
+          </div>
+        </div>
+
+
+        {/* Filter Section */}
+        <div className="flex flex-wrap items-center gap-4">
+          <div className="flex items-center gap-2">
+            <label className="text-sm font-semibold">FROM:</label>
+            <input type="date" className="border rounded-lg px-3 py-2" />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <label className="text-sm font-semibold">TO:</label>
+            <input type="date" className="border rounded-lg px-3 py-2" />
+          </div>
+
+          <button className="flex items-center gap-2 rounded-lg bg-red-600 text-gray-50 hover:bg-red-300 px-4 py-2">
+            <RotateCcw size={18} /> Reset Filter
+          </button>
+        </div>
+      </div>
+
+      {/* Cards Section*/}
+      <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-5 mt-6">
+        {stats
+          .filter((item) => item.amount > 0)
+          .map((item) => {
+            const Icon = item.icon;
+
+            return (
+              <div key={item.title} className="bg-white rounded-2xl border shadow-sm p-6 flex justify-between items-center">
+                <div>
+                  <p className="text-lg uppercase tracking-wider font-bold">{item.title}</p>
+                  <h2 className="text-sm text-slate-500 font-semibold mt-2">{item.amount.toLocaleString()} MMK</h2>
+                </div>
+
+                <div className={`w-14 h-14 rounded-xl ${item.bg} flex items-center justify-center`}>
+                  <Icon className={item.color} size={24} />
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+
+      {/* Bottom Section*/}
+      <div className="grid xl:grid-cols-3 gap-6 mt-8">
+        {/* Best Sellers Items Section */}
+        <div className="xl:col-span-2 bg-white rounded-2xl border shadow-sm p-6">
+          <div className="flex items-center gap-3 mb-8">
+            <ChartColumn className="text-emerald-500" size={28} />
+            <h2 className="font-bold text-xl">Best Seller Items</h2>
+          </div>
+
+          {sortedProducts.map((product, index) => {
+            const percent = totalQty > 0 ? (product.qty / totalQty) * 100 : 0;
+
+            return (
+              <div key={product.name} className="mb-7">
+                <div className="flex justify-between mb-2">
+                  <div className="flex items-center gap-3">
+                    <div className="w-6 h-6 rounded-full bg-green-200 flex items-center justify-center text-xs font-semibold">{index + 1}</div>
+                    <span className="font-medium">{product.name}</span>
+                  </div>
+                  <span className="font-semibold text-green-500">{product.qty} Units ({percent.toFixed(1)}%)</span>
+                </div>
+
+                <div className="w-full bg-slate-200 rounded-full h-4 overflow-hidden">
+                  <div className={`${product.color} h-4 rounded-full transition-all duration-500`} style={{ width: `${percent}%` }} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* Low Stock Warning Section */}
+        <div className="bg-white rounded-2xl border shadow-sm p-6">
+          <div className="flex items-center gap-3 mb-6">
+            <TriangleAlert className="text-red-500" size={28} />
+            <h2 className="font-bold text-xl">Low Stock Warning</h2>
+          </div>
+
+          {lowStockProducts.length > 0 ? (
+            <div className="space-y-4">
+              {lowStockProducts.map((product, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-between rounded-xl border border-red-100 bg-red-50 p-4"
+                >
+                  <div>
+                    <h3 className="font-semibold text-slate-800">
+                      {product.name}
+                    </h3>
+
+                    <p className="text-sm text-slate-500">
+                      Remaining Stock
+                    </p>
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <span className="rounded-full bg-red-100 px-3 py-1 text-sm font-bold text-red-600">
+                      {product.stock} Left
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-[350px]">
+              <div className="w-20 h-20 rounded-full bg-green-100 flex items-center justify-center">
+                <TriangleAlert className="text-green-600" size={36} />
+              </div>
+
+              <h3 className="mt-5 text-lg font-semibold text-slate-700">
+                Great!
+              </h3>
+
+              <p className="text-slate-500">
+                All products are fully stocked.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend-react/src/pages/cashier/History.jsx
+++ frontend-react/src/pages/cashier/History.jsx
@@ -1,179 +1,189 @@
 import { useMemo, useState } from "react";
 
 const salesData = [
-    {
-        id: 6002,
-        date: "2026-06-07 11:30:10",
-        total: 41000,
-        discount: -1000,
-        grandTotal: 40000,
-        payments: [{ type: "KPay"},],
-        status: "COMPLETED",
-    },
-    {
-        id: 6001,
-        date: "2026-06-07 09:15:30",
-        total: 24000,
-        discount: 0,
-        grandTotal: 24000,
-        payments: [{ type: "Cash"}],
-        status: "VOIDED",
-        report: "Customer requested cancellation because of duplicate payment."
-    },
+  {
+    id: 6002,
+    date: "2026-06-07 11:30:10",
+    total: 41000,
+    discount: -1000,
+    grandTotal: 40000,
+    payments: [{ type: "KPay" }],
+    status: "COMPLETED",
+  },
+  {
+    id: 6001,
+    date: "2026-06-07 09:15:30",
+    total: 24000,
+    discount: 0,
+    grandTotal: 24000,
+    payments: [{ type: "Cash" }],
+    status: "VOIDED",
+    report:
+      "Customer requested cancellation because of duplicate payment.",
+  },
 
-    // Dummy rows
-    ...Array.from({ length: 15 }, (_, i) => ({
-        id: 6000 - i,
-        date: "2026-06-06 10:00:00",
-        total: 30000,
-        discount: -1000,
-        grandTotal: 29000,
-        payments: [{ type: "Cash"}],
-        status: "COMPLETED",
-    })),
+  ...Array.from({ length: 15 }, (_, i) => ({
+    id: 6000 - i,
+    date: "2026-06-06 10:00:00",
+    total: 30000,
+    discount: -1000,
+    grandTotal: 29000,
+    payments: [{ type: "Cash" }],
+    status: "COMPLETED",
+  })),
 ];
 
-const ROWS_PER_PAGE = 5;
-
 export default function History() {
-    const [page, setPage] = useState(1);
-    const [saleToDelete, setSaleToDelete] = useState(null);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [saleToDelete, setSaleToDelete] = useState(null);
 
-    const totalPages = Math.ceil(salesData.length / ROWS_PER_PAGE);
+  const totalPages = Math.ceil(salesData.length / rowsPerPage);
+  const currentData = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    return salesData.slice(start, start + rowsPerPage);
+  }, [page, rowsPerPage]);
 
-    const currentData = useMemo(() => {
-        const start = (page - 1) * ROWS_PER_PAGE;
-        return salesData.slice(start, start + ROWS_PER_PAGE);
-    }, [page]);
+  return (
+    <div className="min-h-screen">
+      <div className="overflow-hidden rounded-2xl border border-green-100 bg-white shadow">
 
-    return (
-        <div className="min-h-screen">
-            <div className="overflow-hidden rounded-2xl border border-green-100 bg-white shadow">
-                {/* Table */}
-                <div className="overflow-x-auto">
-                    <table className="w-full">
-                        <thead className="bg-slate-50 text-md text-gray-700">
-                            <tr>
-                                <th className="px-6 py-4 text-left">ID</th>
-                                <th className="px-6 py-4 text-left">Date & Time</th>
-                                <th className="px-6 py-4 text-left">Total</th>
-                                <th className="px-6 py-4 text-left">Discount</th>
-                                <th className="px-6 py-4 text-left">Grand Total</th>
-                                <th className="px-6 py-4 text-left">Payment</th>
-                                <th className="px-6 py-4 text-left">Status</th>
-                                <th className="px-6 py-4 text-left">Action</th>
-                            </tr>
-                        </thead>
+        {/* Header */}
+        <div className="flex flex-col gap-4 border-b bg-slate-50 px-6 py-4 md:flex-row md:items-center md:justify-between">
+          <h2 className="text-xl font-bold text-slate-700">Sales History List</h2>
 
-                        <tbody className="text-sm text-slate-600 font-semibold">
-                            {currentData.map((sale) => (
-                                <tr
-                                    key={sale.id}
-                                    className={`border-t transition hover:bg-slate-50 
-                                        ${sale.status === "VOIDED" ? "line-through text-slate-400 bg-slate-100" : ""}`}
-                                >
-                                    <td className="px-6 py-5 font-bold">#{sale.id}</td>
-                                    <td className="px-6 py-5 whitespace-nowrap">{sale.date}</td>
-                                    <td className="px-6 py-5 ">{sale.total.toLocaleString()}</td>
-                                    <td className="px-6 py-5 text-red-500">{sale.discount.toLocaleString()}</td>
-                                    <td className="px-6 py-5 ">{sale.grandTotal.toLocaleString()}</td>
-                                    <td className="px-6 py-5 ">
-                                        <div className="flex flex-wrap gap-2">
-                                            {sale.payments.map((payment, index) => (
-                                                <span
-                                                    key={index}
-                                                    className={` ${payment.type === "Cash" ? "text-yellow-400" : "text-blue-400" }`}
-                                                >
-                                                    {payment.type}
-                                                </span>
-                                            ))}
-                                        </div>
-                                    </td>
+          <div className="flex items-center gap-2">
+            <span className="text-sm text-gray-600">Show</span>
 
-                                    <td className="px-6 py-5">
-                                        {sale.status === "VOIDED" ? (
-                                            <span className=" text-red-400">{sale.status}</span>
-                                        ) : (
-                                            <span className=" text-green-600">{sale.status}</span>
-                                        )}
-                                    </td>
-
-                                    <td className="px-6 py-5">
-                                        {sale.status === "VOIDED" ? (
-                                            <span className="block max-w-[180px] py-2 truncate font-medium text-slate-400"
-                                                title={sale.report}
-                                            >
-                                                {sale.report}
-                                            </span>
-                                        ) : (
-                                            <button onClick={() => setSaleToDelete(sale)}
-                                                className="rounded-lg bg-red-500 px-4 py-2 text-sm text-white hover:bg-red-400"
-                                            >
-                                                Delete
-                                            </button>
-                                        )}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-                
-                {/* Delete Confirmation Modal */}
-                {saleToDelete && (
-                    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
-                        <div className="w-96 rounded-xl bg-white p-6 shadow-xl">
-                            <h2 className="text-lg font-semibold text-center text-gray-800">Confirm Delete</h2>
-
-                            <p className="mt-3 text-sm text-gray-800">
-                                Are you sure you want to delete sale{" "}
-                                <span className="font-semibold">#{saleToDelete.id}</span> ?
-                            </p>
-
-                            <div className="mt-6 flex justify-end gap-3">
-                                <button onClick={() => setSaleToDelete(null)}
-                                    className="rounded-lg bg-gray-700 px-4 py-2 text-white hover:bg-gray-500"
-                                >
-                                    Cancel
-                                </button>
-
-                                <button onClick={() => setSaleToDelete(null)}
-                                    className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-                                >
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
-                {/* Pagination */}
-                <div className="flex items-center justify-between border-t px-6 py-4">
-                    <button onClick={() => setPage((p) => p - 1)} disabled={page === 1}
-                        className="rounded-lg border px-4 py-2 text-sm disabled:cursor-not-allowed disabled:opacity-40"
-                    >
-                        Previous
-                    </button>
-
-                    <div className="flex gap-2">
-                        {Array.from({ length: totalPages }).map((_, index) => (
-                            <button
-                                key={index}
-                                onClick={() => setPage(index + 1)}
-                                className={`h-10 w-10 rounded-lg  transition ${page === index + 1 ? "bg-blue-600 text-white" : "border hover:bg-slate-100"}`}
-                            >
-                                {index + 1}
-                            </button>
-                        ))}
-                    </div>
-
-                    <button onClick={() => setPage((p) => p + 1)} disabled={page === totalPages}
-                        className="rounded-lg border px-4 py-2 text-sm disabled:cursor-not-allowed disabled:opacity-40"
-                    >
-                        Next
-                    </button>
-                </div>
-            </div>
+            <select value={rowsPerPage}
+              onChange={(e) => {setRowsPerPage(Number(e.target.value));setPage(1);}}
+              className="rounded-lg border px-3 py-2 focus:outline-none focus:border-green-600">
+              <option value={5}>5</option>
+              <option value={10}>10</option>
+              <option value={15}>15</option>
+              <option value={20}>20</option>
+              <option value={25}>25</option>
+            </select>
+            
+            <span className="text-sm text-gray-600 mx-2">records</span>
+          </div>
         </div>
-    );
+
+        {/* Table */}
+
+        <div className="overflow-x-auto">
+          <table className="w-full">
+
+            <thead className="bg-slate-50 text-gray-700">
+              <tr>
+                <th className="px-6 py-4 text-left">ID</th>
+                <th className="px-6 py-4 text-left">Date & Time</th>
+                <th className="px-6 py-4 text-left">Total</th>
+                <th className="px-6 py-4 text-left">Discount</th>
+                <th className="px-6 py-4 text-left">Grand Total</th>
+                <th className="px-6 py-4 text-left">Payment</th>
+                <th className="px-6 py-4 text-left">Status</th>
+                <th className="px-6 py-4 text-left">Action</th>
+              </tr>
+            </thead>
+
+            <tbody className="text-sm font-semibold text-slate-600">
+              {currentData.map((sale) => (
+                <tr
+                    key={sale.id} className={`border-t hover:bg-slate-50 ${
+                    sale.status === "VOIDED" ? "bg-slate-100 text-slate-400 line-through" : ""}`}
+                >
+                    <td className="px-6 py-5 font-bold">#{sale.id}</td>
+                    <td className="px-6 py-5 whitespace-nowrap">{sale.date}</td>
+                    <td className="px-6 py-5">{sale.total.toLocaleString()} </td>
+                    <td className="px-6 py-5 text-red-500">{sale.discount.toLocaleString()}</td>
+                    <td className="px-6 py-5">{sale.grandTotal.toLocaleString()}</td>
+                    <td className="px-6 py-5">
+                        <div className="flex flex-wrap gap-2">
+                        {sale.payments.map((payment, index) => (
+                            <span key={index} className={payment.type === "Cash" ? "text-yellow-500" : "text-blue-500"}>
+                            {payment.type}
+                            </span>
+                        ))}
+                        </div>
+                    </td>
+
+                    <td className="px-6 py-5">
+                        <span className={sale.status === "VOIDED" ? "text-red-500" : "text-green-600"}>{sale.status}</span>
+                    </td>
+
+                    <td className="px-6 py-5">
+                        {sale.status === "VOIDED" ? 
+                            (<span className="block max-w-[180px] truncate text-slate-400" title={sale.report}>{sale.report}</span>) : 
+                            (<button onClick={() => setSaleToDelete(sale)} 
+                                className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600">
+                                Delete
+                            </button>)
+                        }
+                    </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Delete Modal */}
+        {saleToDelete && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+            <div className="w-96 rounded-xl bg-white p-6 shadow-xl">
+              <h2 className="text-center text-lg font-bold">Confirm Delete</h2>
+              <p className="mt-4 text-gray-600">
+                Are you sure you want to delete sale
+                <span className="font-semibold">{" "}#{saleToDelete.id}</span>?
+              </p>
+
+              <div className="mt-6 flex justify-end gap-3">
+                <button onClick={() => setSaleToDelete(null)} className="rounded-lg bg-gray-500 px-4 py-2 text-white">Cancel</button>
+
+                <button
+                  onClick={() => {
+                    // delete api here
+                    setSaleToDelete(null);
+                    }}
+                  className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600">Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Footer */}
+        <div className="flex flex-col gap-4 border-t px-6 py-4 md:flex-row md:items-center md:justify-between">
+          <div className="text-sm text-gray-600">Showing{" "}
+            <span className="font-semibold">{(page - 1) * rowsPerPage + 1}</span>{" "}-
+            <span className="font-semibold">{" "}{Math.min(page * rowsPerPage, salesData.length)}</span>{" "}of{" "}
+            <span className="font-semibold">{salesData.length}</span>{" "}records
+          </div>
+
+          <div className="flex items-center gap-2">
+            <button disabled={page === 1}
+              onClick={() => setPage((p) => p - 1)}
+              className="rounded-lg border px-4 py-2 disabled:cursor-not-allowed disabled:opacity-40"
+            >Previous</button>
+
+            {Array.from({ length: totalPages }).map((_, index) => (
+                <button
+                    key={index} onClick={() => setPage(index + 1)}
+                    className={`h-10 w-10 rounded-lg transition ${
+                    page === index + 1 ? "bg-green-600 text-white" : "border hover:bg-slate-100"}`}
+                >
+                    {index + 1}
+                </button>
+            ))}
+
+            <button disabled={page === totalPages} onClick={() => setPage((p) => p + 1)}
+              className="rounded-lg border px-4 py-2 disabled:cursor-not-allowed disabled:opacity-40"
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
